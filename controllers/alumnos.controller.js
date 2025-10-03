ad57bf927deb5c5b0ecd4ac521643b8e2172ded9
angel alonso mendoza vazquez <43243604+angelalonso111@users.noreply.github.com>
2025-10-03 16:16:36 -0600

    Update alumnos.controller.js

--- controllers/alumnos.controller.js
+++ controllers/alumnos.controller.js
@@ -28,3 +28,36 @@ export const createAlumno = async (req, res) => {
     res.status(500).json({ ok: false, error: err.message });
   }
 };
+
+
+
+
+export const getMaterias = async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT * FROM materias");
+    res.json(rows);
+  } catch (err) {
+    console.error("❌ Error al obtener las materias:", err);
+    res.status(500).json({ ok: false, error: err.message });
+  }
+};
+
+export const createMaterias = async (req, res) => {
+  const { nombre, correo } = req.body;
+
+  if (!nombre || !correo) {
+    return res.status(400).json({ ok: false, error: "Faltan campos: Nombre, carrera y grado son requeridos" });
+  }
+
+  try {
+    const [result] = await pool.query(
+      "INSERT INTO materias (nombre, carrera, grado) VALUES (?, ?, ?)",
+      [nombre, correo]
+    );
+    res.json({ ok: true, id: result.insertId, mensaje: "Materia agregada correctamente" });
+  } catch (err) {
+    console.error("❌ Error insertando Materias:", err);
+    res.status(500).json({ ok: false, error: err.message });
+  }
+};
+
