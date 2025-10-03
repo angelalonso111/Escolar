2237f0c730056a748103fe9203c246a1d4a00799
angel alonso mendoza vazquez <43243604+angelalonso111@users.noreply.github.com>
2025-10-03 16:16:52 -0600

    Update alumnos.routes.js

--- routes/alumnos.routes.js
+++ routes/alumnos.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { getAlumnos, createAlumno } from "../controllers/alumnos.controller.js";
+import { getAlumnos, createAlumno, getMaterias, createMaterias } from "../controllers/alumnos.controller.js";
 
 const router = Router();
 
 router.get("/", getAlumnos);
 router.post("/", createAlumno);
+
+router.get("/", getMaterias);
+router.post("/", createMaterias);
 
 export default router;
