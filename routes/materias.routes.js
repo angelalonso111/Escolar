daa3c82830f11930d598dc87e27d2198367eec77
angel alonso mendoza vazquez <43243604+angelalonso111@users.noreply.github.com>
2025-10-06 16:24:16 -0600

    Update materias.routes.js

--- routes/materias.routes.js
+++ routes/materias.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getMaterias, createMateria } from "../controllers/materias.controller.js";
+import { getMaterias, createMateria, asignarMateriasAlumno } from "../controllers/materias.controller.js";
 
 const router = Router();
 
 router.get("/", getMaterias);
 router.post("/", createMateria);
+router.post("/asignarMaterias", asignarMateriasAAlumno);
 
 export default router;
