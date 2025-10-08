a8c9148449af1f59dacb382d94146931bc9766fc
angel alonso mendoza vazquez <43243604+angelalonso111@users.noreply.github.com>
2025-10-08 16:25:59 -0600

    Update usuarios.routes.js

--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getUsuarios, createUsuarios } from "../controllers/usuarios.controller.js";
+import { getUsuarios, createUsuarios, loginUsuario } from "../controllers/usuarios.controller.js";
 
 const router = Router();
 
 router.get("/", getUsuarios);
 router.post("/", createUsuarios);
+router.post("/login", loginUsuario);
 
 export default router;
